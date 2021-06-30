55091b759464856155931bda96a65fcd76aefa37
J-81 <jonathan.d.oribello@gmail.com>
2021-06-30 13:32:02 -0700

    Added in handling for sample names with spaces. These are sourced from ISA and spaces are converted to "_" now.  This is consistent with runsheet generation which dictates file names.

--- bin/deseq2_normcounts_noERCC_DGE_vis_ISA.R
+++ bin/deseq2_normcounts_noERCC_DGE_vis_ISA.R
@@ -93,8 +93,13 @@ rm(contrast.names)
 files <- list.files(path = "Rsem_gene_counts", pattern = ".genes.results", full.names = TRUE)
 
 ## Reorder the *genes.results files to match the ordering of the ISA samples
-files <- files[sapply(rownames(study), function(x)grep(x, files, value=FALSE, fixed=TRUE))]
-names(files) <- rownames(study)
+
+# Replace spaces in sample names from ISA with "_", consistent with runsheet generation
+library(stringr)
+samples = str_replace_all(rownames(study), " ", "_")
+
+files <- files[sapply(samples, function(x)grep(x, files, value=FALSE, fixed=TRUE))]
+names(files) <- samples
 txi.rsem <- tximport(files, type = "rsem", txIn = FALSE, txOut = FALSE)
 
 
