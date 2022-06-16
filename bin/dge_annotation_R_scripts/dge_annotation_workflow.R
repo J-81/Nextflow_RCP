557fc6ea5b88d50f1542a56bc6d85eacef6b70e3
Jonathan Oribello <jonathan.d.oribello@nasa.gov>
2022-06-16 12:12:01 -0700

    Adding in first portions of reworked R deseq2/gene annotation portion

---
+++ bin/dge_annotation_R_scripts/dge_annotation_workflow.R
@@ -0,0 +1,103 @@
+#! /usr/bin/env -S Rscript --vanilla
+here::i_am("dge_annotation_R_scripts/dge_annotation_workflow.R")
+library("optparse")
+library("here")
+library("cli")
+
+parser <- OptionParser()
+parser <- add_option(parser, c("-v", "--verbose"),
+    action = "store_true",
+    default = FALSE, help = "Print extra output [default]"
+)
+parser <- add_option(parser, c("--skip_perform_dge"),
+    action = "store_true", default = FALSE,
+    help = "Skips running the DGE, this can be used when the output from the DGE already exist",
+)
+parser <- add_option(parser, c("--skip_gene_annotation"),
+    action = "store_true", default = FALSE,
+    help = "Skips running the gene annotation",
+)
+parser <- add_option(parser, c("--work_dir"),
+    default = getwd(),
+    help = "Working directory for all analysis",
+)
+parser <- add_option(parser, c("--input_gene_results_dir"),
+    help = "Directory containing .genes.results files for each sample",
+)
+parser <- add_option(parser, c("--primary_keytype"),
+    help = "Gene ID used for annotation mapping. Often 'ENSEMBL' or 'TAIR' for most GLDS datasets",
+)
+parser <- add_option(parser, c("--normalization"),
+    help = "Normalization approach. Allowed Values: ['default','ERCC-groupB']",
+)
+parser <- add_option(parser, c("--normalized_counts_output_prefix"),
+    help = "output directory and prefix for normalized counts and related files",
+)
+parser <- add_option(parser, c("--dge_output_prefix"),
+    help = "output directory and prefix for dge tables and related files",
+)
+parser <- add_option(parser, c("--DEBUG_MODE_LIMIT_GENES"),
+    default = FALSE, action = "store_true",
+    help = "Performs analysis on last 150 genes",
+)
+parser <- add_option(parser, c("--DEBUG_MODE_ADD_DUMMY_COUNTS"),
+    default = FALSE, action = "store_true",
+    help = "Replaces all gene counts with random values from 0 to 5000",
+)
+parser <- add_option(parser, c("--isa_path"),
+    help = "ISA Archive path, one of two allowed metadata inputs, exactly one metadata input must be supplied",
+)
+parser <- add_option(parser, c("--runsheet_path"),
+    help = "runsheet csv path, one of two allowed metadata inputs, exactly one metadata input must be supplied",
+)
+parser <- add_option(parser, c("--annotation_file_path"),
+    help = "Annotation database file to use for adding gene annotations",
+)
+
+args <- parse_args(parser)
+
+# TODO: add mandatory argument checking
+
+cat_bullet(ansi_align(names(args)), " : ", args)
+
+if (!args$skip_perform_dge) {
+    cli_alert_warning("Running Perform_DGE.Rmd")
+    rmarkdown::render(here("dge_annotation_R_scripts", "Perform_DGE.Rmd"),
+        quiet = !args$verbose,
+        params = list(
+            work_dir = args$work_dir,
+            verbose = args$verbose,
+            input_gene_results_dir = args$input_gene_results_dir,
+            primary_keytype = args$primary_keytype,
+            runsheet_path = args$runsheet_path,
+            isa_path = args$isa_path,
+            normalization = args$normalization,
+            dge_output_prefix = args$dge_output_prefix,
+            normalized_counts_output_prefix = args$normalized_counts_output_prefix,
+            DEBUG_MODE_LIMIT_GENES = args$DEBUG_MODE_LIMIT_GENES,
+            DEBUG_MODE_ADD_DUMMY_COUNTS = args$DEBUG_MODE_ADD_DUMMY_COUNTS
+        )
+    )
+    cli_alert_success("Done running Perform_DGE.Rmd")
+} else {
+    cli_alert_warning("Skipping Perform_DGE.Rmd")
+}
+
+if (!args$skip_gene_annotation) {
+    cli_alert_warning("Running Add_Gene_Annotations.Rmd")
+    rmarkdown::render(here("dge_annotation_R_scripts", "Add_Gene_Annotations.Rmd"),
+        quiet = !args$verbose,
+        params = list(
+            input_table_path = paste0(args$dge_output_prefix, "differential_expression_no_annotations.csv"),
+            work_dir = args$work_dir,
+            annotation_file_path = args$annotation_file_path,
+            primary_keytype = args$primary_keytype,
+            annotated_output_prefix = args$dge_output_prefix
+        )
+    )
+    cli_alert_success("Done running Add_Gene_Annotations.Rmd")
+} else {
+    cli_alert_warning("Skipping Add_Gene_Annotations.Rmd")
+}
+
+cli_alert_success("All done!")
